ce3e9a03b59ce791d5d8cd1cd77d2aefb12df1c4
arthur_x1 <arthur.deygin@domain.com.au>
2018-07-03 16:01:30 +1000

    Adding package to PyPi for dockerization and easier deployment.

---
+++ setup.py
@@ -0,0 +1,31 @@
+# from https://github.com/lyst/lightfm/blob/master/setup.py
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ml_recsys_tools',
+    version='0.5.0',
+    description='Tools for recommendation systems development',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/DomainGroupOSS/ml-recsys-tools',
+    author='Domain group (Arthur Deygin)',
+    author_email='arthurdgn@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='recommendations machine learning',  # Optional
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    install_requires=['requests', 'scipy', 'numpy', 'pandas', 'scikit_learn', 'lightfm',
+                      'scikit_optimize', 'gmaps', 'boto3', 'redis', 'sklearn_pandas'],
+)
