a65f9cbbd1d7e1b2de46477f7e4b3a75c80100eb
arthur_x1 <arthur.deygin@domain.com.au>
2018-11-02 14:53:37 +1100

    Observations handler data_info function.

--- ml_recsys_tools/data_handlers/interaction_handlers_base.py
+++ ml_recsys_tools/data_handlers/interaction_handlers_base.py
@@ -1,4 +1,5 @@
 import warnings
+from types import SimpleNamespace
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -84,6 +85,16 @@ class ObservationsDF(LogCallsTimeAndOutput):
             return self.df_obs[self.iid_col].value_counts()
         else:
             raise ValueError('Observations dataframe is empty')
+
+    def data_info(self):
+        rating_pctl = np.percentile(self.ratings, [20, 80])
+        return SimpleNamespace(**{
+            'len': len(self.df_obs),
+            'n_unique_users': len(np.unique(self.user_ids)),
+            'n_unique_items': len(np.unique(self.item_ids)),
+            'ratings_20_pctl': rating_pctl[0],
+            'ratings_80_pctl': rating_pctl[1],
+        })
 
     def sample_observations(self,
                             n_users=None,
--- tests/test_movielens_data.py
+++ tests/test_movielens_data.py
@@ -21,3 +21,12 @@ class TestMovieLens(unittest.TestCase):
         movies_df = pd.read_csv(movies_csv_path)
         self.assertListEqual(list(movies_df.columns), ['item_ind', 'itemid', 'genres'])
         self.assertEqual(len(movies_df), 3883)
+
+        from ml_recsys_tools.data_handlers.interaction_handlers_base import ObservationsDF
+        obs = ObservationsDF(df_obs=ratings_df)
+        info = obs.data_info()
+        self.assertEqual(info.len, 989539)
+        self.assertEqual(info.n_unique_items, 3706)
+        self.assertEqual(info.n_unique_users, 5796)
+        self.assertEqual(info.ratings_20_pctl, 3.0)
+        self.assertEqual(info.ratings_80_pctl, 5.0)
